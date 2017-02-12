c43bf6bdea844a489a8b699c040b90bf5ca9d9cd
STMiki <micou.veillet@gmail.com>
2017-02-12 21:27:06 +0100

    test console
    parceque la flemme de me remmetre a tkinter

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
 # -*- coding: utf-8 -*-
 import os
 import time
-
 from bin.manga import *
 from bin.Notification import Notification
 
-def main():
-	"""main function"""
+HELP = "Bienvenue dans manga notifier !\nTapez help pour plus d'aide.\n(La séparation se fait avec des virgule ex: add,manga name,url)\n"
+
+def start():
+	"""start function"""
 	test_manga()
 	time.sleep(60*5)
 	os.system("cls")
 
+def console():
+	commands = ['help', 'add', 'start']
+	temp = input(">>>")
+	command = temp.split(",")
+	if command[0] in commands:
+		if command[0] == commands[0]: #help
+			print(HELP)
+			return console()
+		elif command[0] == commands[1]: #add
+			if len(command) == 3 and (command[2].startswith("http://") or command[2].startswith("https://")):
+				ajout_parametres(command[1], command[2])
+				return console()
+		elif command[0] == commands[2]: #start
+			os.system("cls")
+			while True:
+				start()
+	else:
+		print(HELP)
+
 if not os.path.exists('save.manga'):
 	open('save.manga', 'wb')
 
+if __name__ == "__main__":
+	console()
+
+#.withdraw()
+#Hides the window. Restore it with .deiconify() or .iconify().
+
+
+
+"""
 ajout_parametres("Black Clover", "http://www.japscan.com/mangas/black-clover/")
 ajout_parametres("Twin Star Exorcists", "http://www.crunchyroll.com/twin-star-exorcists")
 ajout_parametres("Doupo Cangqiong", "http://mangafox.me/manga/doupo_cangqiong/")
@@ -24,12 +53,4 @@ ajout_parametres("Wizardly Tower", "http://mangafox.me/manga/wizardly_tower/")
 ajout_parametres("Black Healer", "http://mangafox.me/manga/isekai_de_kuro_no_iyashi_te_tte_yobarete_imasu")
 ajout_parametres("Wu Dong Qian Kun", "http://mangafox.me/manga/wu_dong_qian_kun/")
 ajout_parametres("Gosu", "http://mangafox.me/manga/gosu/")
-
-time.sleep(2)
-os.system("cls")
-
-while True:
-	main()
-
-#.withdraw()
-#Hides the window. Restore it with .deiconify() or .iconify().
+"""
